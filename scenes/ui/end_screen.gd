db1783c633147cadb356ca387b86bc594c915e7a
Jon Lake <francis.j.lake@gmail.com>
2023-08-10 16:29:59 -0400

    Add victory and defeat sfx

--- scenes/ui/end_screen.gd
+++ scenes/ui/end_screen.gd
@@ -18,9 +18,16 @@ func _ready():
 func set_defeat():
 	$%TitleLabel.text = "Defeat"
 	$%DescriptionLabel.text = "You lost!"
+	play_jingle(true)
 	
 	
-	
+func play_jingle(defeat: bool = false):
+	if defeat:
+		$DefeatStreamPlayer.play()
+	else:
+		$VictoryStreamPlayer.play()
+
+
 func on_restart_button_pressed():
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
