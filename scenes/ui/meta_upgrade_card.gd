aae3f5903d124e58b40100a5d2ce17e166879813
Jon Lake <francis.j.lake@gmail.com>
2023-08-19 18:44:24 -0400

    Improve the meta upgrade card

--- scenes/ui/meta_upgrade_card.gd
+++ scenes/ui/meta_upgrade_card.gd
@@ -5,6 +5,7 @@ extends PanelContainer
 @onready var progress_bar = $%ProgressBar
 @onready var purchase_button = $%PurchaseButton
 @onready var progress_label = $%ProgressLabel
+@onready var count_label = $%CountLabel
 
 
 var upgrade: MetaUpgrade
@@ -14,6 +15,7 @@ func _ready():
 	purchase_button.pressed.connect(on_purchase_pressed)
 
 	
+
 func set_meta_upgrade(upgrade: MetaUpgrade):
 	self.upgrade = upgrade
 	name_label.text = upgrade.title
@@ -22,18 +24,24 @@ func set_meta_upgrade(upgrade: MetaUpgrade):
 
 
 func update_progress():
+	var current_quantity = 0
+	if MetaProgression.save_data["meta_upgrades"].has(upgrade.id):
+		current_quantity = MetaProgression.save_data["meta_upgrades"][upgrade.id]["quantity"]
+		
+	var is_maxed = current_quantity >= upgrade.max_quantity
 	var currency = MetaProgression.save_data["meta_upgrade_currency"]
 	var percent = currency / upgrade.experience_cost
 	percent = min(percent, 1)
 	progress_bar.value = percent
-	purchase_button.disabled = percent < 1
+	purchase_button.disabled = percent < 1 || is_maxed
+	if is_maxed:
+		purchase_button.text = "Max"
 	progress_label.text = str(currency) + "/" + str(upgrade.experience_cost)
+	count_label.text = "x%d" % current_quantity
 
 
 func select_card():
 	$AnimationPlayer.play("selected")
-
-
 
 
 func on_purchase_pressed():
