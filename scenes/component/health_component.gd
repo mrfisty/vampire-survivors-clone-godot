a082919709a244819d3e6e97c4a643649b1c769e
Jon Lake <francis.j.lake@gmail.com>
2023-08-20 22:39:34 -0400

    Add health regeneration meta upgrade

--- scenes/component/health_component.gd
+++ scenes/component/health_component.gd
@@ -3,6 +3,7 @@ class_name HealthComponent
 
 signal died
 signal health_changed
+signal health_decreased
 
 @export var max_health: float = 10
 var current_health
@@ -11,13 +12,20 @@ var current_health
 func _ready():
 	current_health = max_health
 	
-
+	
 func damage(damage_amount: float):
+	current_health = clamp(current_health - damage_amount, 0, max_health)
 	current_health = max(current_health - damage_amount, 0)
 	health_changed.emit()
+	if damage_amount > 0:
+		health_decreased.emit()
 	Callable(check_death).call_deferred()
 	
-	
+
+func heal(heal_amount: int):
+	damage(-heal_amount)
+
+
 func get_health_percent():
 	if max_health <= 0:
 		return 0
